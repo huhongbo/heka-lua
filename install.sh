cb2cad3e13fae50f9c8fff7fccbe2453cb499cfc
huhongbo <huhongbo@gmail.com>
2015-09-18 14:23:53 +0800

    add install script

---
+++ install.sh
@@ -0,0 +1,3 @@
+cp lua_modules/opentsdb_line_protocol.lua /usr/share/heka/lua_modules/
+cp lua_encoders/schema_opentsdb_line.lua /usr/share/heka/lua_encoders/
+cp lua_decoders/json_custom.lua /usr/share/heka/lua_decoders/
